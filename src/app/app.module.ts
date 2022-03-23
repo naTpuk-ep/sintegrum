48d2245bd9b4798e3205f3df6ddcffc1b12411f1
naTpuk-ep <naTpuk.ep@gmail.com>
2022-03-23 16:35:48 +0400

    feat: auth-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AuthComponent } from './auth.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AuthComponent],
+  imports: [CommonModule, MatCardModule, MatFormFieldModule, MatInputModule],
+  exports: [AuthComponent],
 })
-export class AuthModule { }
+export class AuthModule {}
