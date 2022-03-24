cf095786f92f917104ae167494d2d66a5ba6242a
naTpuk-ep <naTpuk.ep@gmail.com>
2022-03-24 21:58:12 +0400

    feat: redirect to auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { AuthGuard } from './shared/auth.guard';
+import { AuthComponent } from './auth/auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { LocalStorageTokenService } from './local-storage-token.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+  constructor(
+    private localStorageTokenService: LocalStorageTokenService,
+    private _router: Router
+  ) {}
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    return true;
+    if (this.localStorageTokenService.get()) {
+      return true;
+    }
+    this._router.navigate(['auth']);
+    return false;
   }
 }
